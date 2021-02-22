a83e4f871103a3136aaa81177d155b62596baac1
Hammad <hammadahmed_98@hotmail.com>
2021-02-22 23:51:35 +0500

    CompletedByHammad

--- users.js
+++ users.js
@@ -1,49 +1,63 @@
 const allUsers = [
-    {
-        id: 100,
-        name: "Qasim Salam"
-    },
-        {
-        id: 101,
-        name: "Neelam Anwar"
-    },
-        {
-        id: 102,
-        name: "Saad Javaid"
-    },
-        {
-        id: 103,
-        name: "Talha Masood"
-    },
-        {
-        id: 104,
-        name: "Ali Bilal"
-    },
-        {
-        id: 105,
-        name: "Zain"
-    },
-    {
-        id: 106,
-        name: "Talal Ali"
-    },
-        {
-        id: 107,
-        name: "Bilal Zain"
-    },
-        {
-        id: 108,
-        name: "Mueez Ali"
-    },
+  {
+    id: 100,
+    name: "Qasim Salam",
+  },
+  {
+    id: 101,
+    name: "Neelam Anwar",
+  },
+  {
+    id: 102,
+    name: "Saad Javaid",
+  },
+  {
+    id: 103,
+    name: "Talha Masood",
+  },
+  {
+    id: 104,
+    name: "Ali Bilal",
+  },
+  {
+    id: 105,
+    name: "Zain",
+  },
+  {
+    id: 106,
+    name: "Talal Ali",
+  },
+  {
+    id: 107,
+    name: "Bilal Zain",
+  },
+  {
+    id: 108,
+    name: "Mueez Ali",
+  },
 ];
 
 const linked = [
-    {
-        id: 106,
-        name: "Talal Ali"
-    },
-    {
-        id: 108,
-        name: "Mueez Ali"
-    },
-]
+  {
+    id: 106,
+    name: "Talal Ali",
+  },
+  {
+    id: 108,
+    name: "Mueez Ali",
+  },
+];
+let arr = [];
+const UnlinkedUsers = (allUsers, linked) => {
+  allUsers.map((item) => {
+    let check = true;
+    linked.map((it) => {
+      if (item.id == it.id && item.name == it.name) {
+        check = false;
+      }
+    });
+    check ? arr.push(item) : null;
+  });
+};
+UnlinkedUsers(allUsers, linked);
+console.log(arr);
